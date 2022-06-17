89014a5296c5c268a7af442e85fd92a859d391c6
jesusoyer <oyervidesjesus017@gmail.com>
2022-06-17 09:32:48 -0500

    installed sequelize package and created config directory

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express")
-
+const sequelize = require('./config/connection')
 
 const app = express()
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 
 app.listen (PORT, ()=> console.log(`${PORT} listening`))
